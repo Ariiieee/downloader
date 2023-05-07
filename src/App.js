e24d625e3e838630ab2d95f8b1951505dabe5bc5
Ariiie <oluwafunmilayodada2018@gmail.com>
2023-05-07 22:50:13 +0100

    added desc

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ const App = () => {
   return (
     <div>
       <h1> Download below:</h1>
+      <p>Paste a url link to download below:</p>
       <input placeholder="paste link here" value={downloadUrl} onChange={(e) => setDownloadUrl(e.target.value)} />
       <button onClick={handleDownloadFile}>Download</button>
 
